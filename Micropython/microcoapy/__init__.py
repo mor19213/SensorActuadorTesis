f33604bc13fbee606be38f6f63f9fabdd751589d
mor19213 <mor19213@uvg.edu.gt>
2023-09-15 22:29:11 -0600

    Orden programas

--- Micropython/microcoapy/__init__.py
+++
@@ -1,3 +0,0 @@
-from .microcoapy import Coap
-from .coap_macros import COAP_CONTENT_FORMAT
-from .coap_macros import COAP_RESPONSE_CODE
--- Micropython/prueba.py
+++
@@ -1,6 +0,0 @@
-class prueba:
-    def __init__(self):
-        self.a = 1
-        self.b = 2
-    def suma(self):
-        return self.a + self.b
